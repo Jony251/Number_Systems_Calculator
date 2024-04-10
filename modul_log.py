b553496babd608ab08622f350400543726cade19
Evgeny <120419536+Jony251@users.noreply.github.com>
2024-04-10 10:46:34 +0300

    Update modul_log.py

--- modul_log.py
+++ modul_log.py
@@ -4,28 +4,29 @@ import modul_system_cal
 
 
 def click(main_window):
-    str_dec = "10"
     str_oct = "8"
     str_hex = "16"
     str_bin = "2"
     try:
         main_window.text_to_show = ""
-        input_t = main_window.input_box.text()
+        input_num_str = main_window.input_box.text()
         input_tipe = main_window.input_base.text()
+        for_dec_base = main_window.input_dec.text()
+
         if main_window.radio_but_dec.isChecked():
-            main_window.text_to_show = modul_system_cal.system_calculator(str_dec, input_t)
+            main_window.text_to_show = modul_system_cal.uncode_system_calculator(for_dec_base, input_num_str)
 
         elif main_window.radio_but_oct.isChecked():
-            main_window.text_to_show = modul_system_cal.system_calculator(str_oct, input_t)
+            main_window.text_to_show = modul_system_cal.system_calculator(str_oct, input_num_str)
 
         elif main_window.radio_but_hex.isChecked():
-            main_window.text_to_show = modul_system_cal.system_calculator(str_hex, input_t)
+            main_window.text_to_show = modul_system_cal.system_calculator(str_hex, input_num_str)
 
         elif main_window.radio_but_bin.isChecked():
-            main_window.text_to_show = modul_system_cal.system_calculator(str_bin, input_t)
+            main_window.text_to_show = modul_system_cal.system_calculator(str_bin, input_num_str)
 
         elif main_window.radio_but_user_base.isChecked():
-            main_window.text_to_show = modul_system_cal.system_calculator(input_tipe, input_t)
+            main_window.text_to_show = modul_system_cal.system_calculator(input_tipe, input_num_str)
 
         else:
             main_window.text_to_show = "There are some problems with the program"
